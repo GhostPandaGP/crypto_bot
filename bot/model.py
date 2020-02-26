48979d969b4ab8466b7ac3e77e05da1ccb01e75e
Varvara Shushkova <your_email@whatever.com>
2020-02-27 02:06:57 +0300

    change load data

--- bot/model.py
+++ bot/model.py
@@ -7,17 +7,21 @@ NOTIFY_PAIR = ['USD-BTC', 'USD-LTC', 'USD-ETH']
 
 def prepare_data(param):
     client = BittrexClient()
+    res_frame = []
     for pair in param:
+
+        print(pair)
         current_price = client.get_ticker(pair=pair)
 
-        res_frame = []
         for row in current_price['result']:
             res_frame.append(row)
             row['usd'] = pair
 
-        usd = pd.DataFrame(res_frame)
+    usd = pd.DataFrame(res_frame)
+    usd.to_csv('usd.csv', sep=';')
 
-        return usd
+    return usd
+
 
 if __name__ == "__main__":
     prepare_data(NOTIFY_PAIR)
